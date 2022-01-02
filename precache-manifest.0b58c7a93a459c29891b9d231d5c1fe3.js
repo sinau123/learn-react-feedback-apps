ee992acab1cdf78e6d133406ebbada0518b5ec14
Denis Eko Harbiyanto <sinau123@gmail.com>
2022-01-02 15:05:16 +0700

    Updates

---
+++ precache-manifest.0b58c7a93a459c29891b9d231d5c1fe3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0cab43e934619c30f3b8a3c32c43614",
+    "url": "/learn-react-feedback-apps/index.html"
+  },
+  {
+    "revision": "d45a9a40a8ed802d3705",
+    "url": "/learn-react-feedback-apps/static/css/main.9e16105f.chunk.css"
+  },
+  {
+    "revision": "78dadad1df4e17014049",
+    "url": "/learn-react-feedback-apps/static/js/2.f7507408.chunk.js"
+  },
+  {
+    "revision": "d18dba208164d01c98054a17fb168340",
+    "url": "/learn-react-feedback-apps/static/js/2.f7507408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73610d0dc110e84390c9",
+    "url": "/learn-react-feedback-apps/static/js/3.b23030b9.chunk.js"
+  },
+  {
+    "revision": "d45a9a40a8ed802d3705",
+    "url": "/learn-react-feedback-apps/static/js/main.7baf70ad.chunk.js"
+  },
+  {
+    "revision": "27435226098bb0ebd46e",
+    "url": "/learn-react-feedback-apps/static/js/runtime-main.e68e2577.js"
+  },
+  {
+    "revision": "d232155c0078cd740770da1fd3daffaf",
+    "url": "/learn-react-feedback-apps/static/media/5000x1500.d232155c.png"
+  }
+]);
